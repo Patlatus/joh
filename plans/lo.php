cfdfd74e1825290c9e6a57c10abbb88575e69fa0
BogdanChykD@gmail.com <BogdanChykD@gmail.com@4bb32916-dce3-c1cb-8543-0904a3ff6446>
2013-08-08 09:10:52 +0000

    Audit log functionality implemented; changes to install SQL script added

--- plans/lo.php
+++ plans/lo.php
@@ -1,6 +1,8 @@
 <?php
-include('initcfg.php');
-unset($_SESSION['username'], $_SESSION['userid']);
-setcookie('planssiteusername', '', time()-100);
-setcookie('planssitepassword', '', time()-100);
+    include('initcfg.php');
+    updateUser($log, 0);
+    unset($_SESSION['username'], $_SESSION['userid']);
+    $_SESSION['userid'] = -1;
+    setcookie('julfysoft_username', '', time() - 100);
+    setcookie('julfysoft_password', '', time() - 100);
 ?>
