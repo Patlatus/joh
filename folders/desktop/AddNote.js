50499b1bb581b7210a509ade3636832994992e1f
BogdanChykD@gmail.com <BogdanChykD@gmail.com@4bb32916-dce3-c1cb-8543-0904a3ff6446>
2013-10-17 13:54:54 +0000

    Fixing bugs from production

--- folders/desktop/AddNote.js
+++ folders/desktop/AddNote.js
@@ -111,7 +111,7 @@ Ext.define('MyDesktop.AddNote', {
         } : null;
         
         if (this.moduleId === 'notepad') {
-            if (window.xmlconfig.lsEnabled) {
+            if (window.xmlconfig.lsEnabled && !window.xmlconfig.guestmode) {
                 this.loadStickers(this.getstickersapi, window.xmlconfig.userid, 0, 50, 'background-color:yellow;', {
                     showDuplicateButton : false,
                     showEditButton      : true,
