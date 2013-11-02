f718cdccafdd4906402fc8f0d419d6189336ba0f
BogdanChykD@gmail.com <BogdanChykD@gmail.com@4bb32916-dce3-c1cb-8543-0904a3ff6446>
2013-11-02 20:15:21 +0000

    Fixed error with HTML escaping encoding

--- trunk/folders/desktop/App.js
+++ trunk/folders/desktop/App.js
@@ -168,7 +168,7 @@ Ext.define('MyDesktop.App', {
         for (var i=0;i<vars.length;i++) {
             var pair = vars[i].split("=");
             if (pair[0] == variable) {
-                return pair[1];
+                return decodeURI(pair[1]);
             }
         } 
         return null;
